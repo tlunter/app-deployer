0382757a480ad8d09a0af4139a00bfb477cb564c
Todd Lunter <tlunter@gmail.com>
2015-08-23 21:01:33 -0400

    Start making a Thor CLI

---
+++ lib/app_deployer/cli.rb
@@ -0,0 +1,32 @@
+require 'app_deployer'
+require 'pry'
+
+module AppDeployer
+  class CLI < Thor
+    class_option :file, type: :string, default: 'Appfile'
+
+    no_commands do
+      def setup
+        AppDeployer::Core::Sandbox.instance.find_and_eval(file_name: options[:file])
+      end
+    end
+
+    desc 'start-application DEPLOY', "start the deploy's application"
+    def start_application(deploy)
+      setup
+
+      deploy = AppDeployer::Core::Sandbox.instance[:deploy][deploy.to_sym]
+      deploy.start_application
+    end
+
+    desc 'update-load-balancer DEPLOY', "update the deploy's load-balancer upstream with current application routes"
+    def update_load_balancer(deploy)
+      setup
+
+      deploy = AppDeployer::Core::Sandbox.instance[:deploy][deploy.to_sym]
+      deploy.assign_app_ports_to_load_balancer
+      deploy.reload_load_balancer
+    end
+  end
+end
+
--- lib/app_deployer/core/sandbox.rb
+++ lib/app_deployer/core/sandbox.rb
@@ -9,6 +9,22 @@ module AppDeployer
         @objects = Hash.new { |hash, key| hash[key] = {} }
       end
 
+      def find_and_eval(file_name:)
+        directory = Dir.pwd
+        file = nil
+
+        until File.dirname(directory) == directory
+          if File.exist?(File.join(directory, file_name))
+            file = File.join(directory, file_name)
+            break
+          else
+            directory = File.dirname(directory)
+          end
+        end
+
+        instance_eval(File.read(file), file) unless file.nil?
+      end
+
       def [](obj_name)
         objects[obj_name]
       end
